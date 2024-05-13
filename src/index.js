298235a5f0f66c35cef1066ec9eab30d08af79fc
Adeola Kehinde <adeolav04@gmail.com>
2024-05-13 22:46:45 +0100

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+    let apiKey = "706t692aeb44c9246o4e0036b9396dbf";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
 function search(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Lagos");
